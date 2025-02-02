b8529e8c95fb91dc7f4df0f89923e59b8d3059e2
Mathijs Futselaar <mathijs@matttt.nl>
2025-02-03 00:43:35 +0100

    frontend

---
+++ theme/script.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelectorAll('.nav-links > a').forEach((element) => {
+        element.setAttribute('data-tooltip', element.title);
+        element.title = '';
+
+        element.addEventListener('mouseenter', (event) => {
+            document.getElementById('nav-links-info').innerText = event.target.getAttribute('data-tooltip');
+        })
+
+        element.addEventListener('mouseleave', (event) => {
+            document.getElementById('nav-links-info').innerText = '';
+        })
+    })
+})
