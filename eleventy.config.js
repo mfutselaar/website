69a2059d9cabc25bb86ac88c04b58bdb6c71a872
Mathijs Futselaar <mathijs@matttt.nl>
2025-07-16 13:54:45 +0200

    46b198f675bd96c0399fc8c3f0c01d3c775ab7d8

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,12 +1,13 @@
 import eleventyNavigationPlugin from "@11ty/eleventy-navigation";
 import syntaxHighlight from "@11ty/eleventy-plugin-syntaxhighlight";
 
-export default function(eleventyConfig) {
+export default function (eleventyConfig) {
 
     eleventyConfig.addPlugin(eleventyNavigationPlugin);
     eleventyConfig.addPlugin(syntaxHighlight);
 
     eleventyConfig.addPassthroughCopy("src/js");
+    eleventyConfig.addPassthroughCopy("src/css");
     eleventyConfig.addPassthroughCopy("src/img");
 
     eleventyConfig.ignores.add("src/index.html");
